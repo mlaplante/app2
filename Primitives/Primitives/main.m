8699105a5d24a2ec8254115e83877ef4884e1bb9
Michael La Plante <michael@michaellaplante.com>
2013-08-31 10:43:20 -0400

    adding week 2 code

---
+++ Primitives/Primitives/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  Primitives
+//
+//  Created by Michael La Plante on 8/31/13.
+//  Copyright (c) 2013 Michael La Plante. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int anInt = 24;
+        NSLog(@"%i",anInt);
+        
+        char charred = 'c';
+        NSLog(@"the letter %c",charred);
+        
+        float f = 2.34;
+        NSLog(@"%f",f);
+        
+    }
+    return 0;
+}
+
